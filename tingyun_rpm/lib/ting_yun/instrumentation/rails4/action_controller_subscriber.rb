98f483d538502a7df2f7da0dd170f277c13ff65d
liuhq <liuhq@ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da>
2016-01-12 05:25:54 +0000

    订阅里加上module Rails4
    git-svn-id: https://192.168.1.42/tingyun-ruby-agent@49 ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da

--- tingyun_rpm/lib/ting_yun/instrumentation/rails4/action_controller_subscriber.rb
+++ tingyun_rpm/lib/ting_yun/instrumentation/rails4/action_controller_subscriber.rb
@@ -7,85 +7,87 @@ require 'ting_yun/agent/transaction'
 
 module TingYun
   module Instrumentation
-    class ActionControllerSubscriber < TingYun::Instrumentation::Support::EventedSubscriber
-      def start(name, id, payload) #THREAD_LOCAL_ACCES
-        state = TingYun::Agent::TransactionState.tl_get
-        request = state.request
-        event = ControllerEvent.new(name, Time.now, nil, id, payload, request)
-        push_event(event)
-        start_transaction(state, event)
-      rescue => e
-        log_notification_error(e, name, 'start')
-      end
-
-      def finish(name, id, payload) #THREAD_LOCAL_ACCESS
-
-        event = pop_event(id)
-        event.payload.merge!(payload)
-
-        state = TingYun::Agent::TransactionState.tl_get
-
-
-        stop_transaction(state, event)
-      rescue => e
-        log_notification_error(e, name, 'finish')
-      end
-
-      def start_transaction(state, event)
-        TingYun::Agent::Transaction.start(state, :controller,
-                                          :request          => event.request,
-                                          :filtered_params  => event.payload[:params],
-                                          :apdex_start_time => event.queue_start,
-                                          :transaction_name => event.metric_name)
-      end
-
-      def stop_transaction(state, event)
-        txn = state.current_transaction
-        TingYun::Agent::Transaction.stop(state)
-      end
-
-
-    end
-
-    class ControllerEvent < TingYun::Instrumentation::Support::Event
-      attr_accessor :parent
-      attr_reader :queue_start, :request
-
-      def initialize(name, start, ending, transaction_id, payload, request)
-        # We have a different initialize parameter list, so be explicit
-        super(name, start, ending, transaction_id, payload,nil)
-
-        @request = request
-        @controller_class = payload[:controller].split('::') \
-            .inject(Object){|m,o| m.const_get(o)}
-
-        if request && request.respond_to?(:env)
-          @queue_start = TingYun::Instrumentation::Support::QueueTime.parse_frontend_timestamp(request.env, self.time)
+    module Rails4
+      class ActionControllerSubscriber < TingYun::Instrumentation::Support::EventedSubscriber
+        def start(name, id, payload) #THREAD_LOCAL_ACCES
+          state = TingYun::Agent::TransactionState.tl_get
+          request = state.request
+          event = ControllerEvent.new(name, Time.now, nil, id, payload, request)
+          push_event(event)
+          start_transaction(state, event)
+        rescue => e
+          log_notification_error(e, name, 'start')
         end
-      end
 
-      def metric_name
-        unless TingYun::Agent.config[:'auto_app_naming']
-          TingYun::Agent.config[:'app_name']
-        else
-          @metric_name ||= "WebAction/Rails/#{metric_path}/#{metric_action}"
+        def finish(name, id, payload) #THREAD_LOCAL_ACCESS
+
+          event = pop_event(id)
+          event.payload.merge!(payload)
+
+          state = TingYun::Agent::TransactionState.tl_get
+
+
+          stop_transaction(state, event)
+        rescue => e
+          log_notification_error(e, name, 'finish')
         end
+
+        def start_transaction(state, event)
+          TingYun::Agent::Transaction.start(state, :controller,
+                                            :request => event.request,
+                                            :filtered_params => event.payload[:params],
+                                            :apdex_start_time => event.queue_start,
+                                            :transaction_name => event.metric_name)
+        end
+
+        def stop_transaction(state, event)
+          txn = state.current_transaction
+          TingYun::Agent::Transaction.stop(state)
+        end
+
+
       end
 
-      def metric_path
-        @controller_class.controller_path
-      end
+      class ControllerEvent < TingYun::Instrumentation::Support::Event
+        attr_accessor :parent
+        attr_reader :queue_start, :request
 
-      def metric_action
-        payload[:action]
-      end
+        def initialize(name, start, ending, transaction_id, payload, request)
+          # We have a different initialize parameter list, so be explicit
+          super(name, start, ending, transaction_id, payload, nil)
 
-      def path
-        path =  payload[:path]
-      end
+          @request = request
+          @controller_class = payload[:controller].split('::') \
+            .inject(Object) { |m, o| m.const_get(o) }
 
-      def to_s
-        "#<TingYun::Instrumentation::ControllerEvent:#{object_id} name: \"#{name}\" id: #{transaction_id} payload: #{payload}}>"
+          if request && request.respond_to?(:env)
+            @queue_start = TingYun::Instrumentation::Support::QueueTime.parse_frontend_timestamp(request.env, self.time)
+          end
+        end
+
+        def metric_name
+          unless TingYun::Agent.config[:'auto_app_naming']
+            TingYun::Agent.config[:'app_name']
+          else
+            @metric_name ||= "WebAction/Rails/#{metric_path}/#{metric_action}"
+          end
+        end
+
+        def metric_path
+          @controller_class.controller_path
+        end
+
+        def metric_action
+          payload[:action]
+        end
+
+        def path
+          path = payload[:path]
+        end
+
+        def to_s
+          "#<TingYun::Instrumentation::ControllerEvent:#{object_id} name: \"#{name}\" id: #{transaction_id} payload: #{payload}}>"
+        end
       end
     end
   end
@@ -108,7 +110,7 @@ TingYun::Support::LibraryDetection.defer do
   end
 
   executes do
-    ::TingYun::Instrumentation::ActionControllerSubscriber \
+    ::TingYun::Instrumentation::Rails4::ActionControllerSubscriber \
       .subscribe(/^process_action.action_controller$/)
   end
 end
--- tingyun_rpm/lib/ting_yun/instrumentation/rails4/active_record_subscriber.rb
+++ tingyun_rpm/lib/ting_yun/instrumentation/rails4/active_record_subscriber.rb
@@ -7,58 +7,59 @@ require 'ting_yun/support/helper'
 
 module TingYun
   module Instrumentation
-    class ActiveRecordSubscriber < TingYun::Instrumentation::Support::EventedSubscriber
-      CACHED_QUERY_NAME = 'CACHE'.freeze unless defined? CACHED_QUERY_NAME
+    module Rails4
+      class ActiveRecordSubscriber < TingYun::Instrumentation::Support::EventedSubscriber
+        CACHED_QUERY_NAME = 'CACHE'.freeze unless defined? CACHED_QUERY_NAME
 
-      def initialize
-        # We cache this in an instance variable to avoid re-calling method
-        # on each query.
-        super
-      end
-
-      def start(name, id, payload) #THREAD_LOCAL_ACCESS
-        return if payload[:name] == CACHED_QUERY_NAME
-        super
-      rescue => e
-        log_notification_error(e, name, 'start')
-      end
-
-      def finish(name, id, payload) #THREAD_LOCAL_ACCESS
-        return if payload[:name] == CACHED_QUERY_NAME
-        event  = pop_event(id)
-        config = active_record_config_for_event(event)
-        record_metrics(event, config)
-      rescue => e
-        log_notification_error(e, name, 'finish')
-      end
-
-      def record_metrics(event, config)
-        base, *other_metrics = TingYun::Instrumentation::Support::ActiveRecordHelper.metrics_for(event.payload[:name],
-                                                                            TingYun::Helper.correctly_encoded(event.payload[:sql]),
-                                                                            config && config[:adapter])
-
-        TingYun::Agent.agent.stats_engine.tl_record_scoped_and_unscoped_metrics(base, other_metrics, event.duration)
-      end
-
-
-      def active_record_config_for_event(event)
-        return unless event.payload[:connection_id]
-
-        connection = nil
-        connection_id = event.payload[:connection_id]
-
-        ::ActiveRecord::Base.connection_handler.connection_pool_list.each do |handler|
-          connection = handler.connections.detect do |conn|
-            conn.object_id == connection_id
-          end
-
-          break if connection
+        def initialize
+          # We cache this in an instance variable to avoid re-calling method
+          # on each query.
+          super
         end
 
-        connection.instance_variable_get(:@config) if connection
+        def start(name, id, payload) #THREAD_LOCAL_ACCESS
+          return if payload[:name] == CACHED_QUERY_NAME
+          super
+        rescue => e
+          log_notification_error(e, name, 'start')
+        end
+
+        def finish(name, id, payload) #THREAD_LOCAL_ACCESS
+          return if payload[:name] == CACHED_QUERY_NAME
+          event = pop_event(id)
+          config = active_record_config_for_event(event)
+          record_metrics(event, config)
+        rescue => e
+          log_notification_error(e, name, 'finish')
+        end
+
+        def record_metrics(event, config)
+          base, *other_metrics = TingYun::Instrumentation::Support::ActiveRecordHelper.metrics_for(event.payload[:name],
+                                                                                                   TingYun::Helper.correctly_encoded(event.payload[:sql]),
+                                                                                                   config && config[:adapter])
+
+          TingYun::Agent.agent.stats_engine.tl_record_scoped_and_unscoped_metrics(base, other_metrics, event.duration)
+        end
+
+
+        def active_record_config_for_event(event)
+          return unless event.payload[:connection_id]
+
+          connection = nil
+          connection_id = event.payload[:connection_id]
+
+          ::ActiveRecord::Base.connection_handler.connection_pool_list.each do |handler|
+            connection = handler.connections.detect do |conn|
+              conn.object_id == connection_id
+            end
+
+            break if connection
+          end
+
+          connection.instance_variable_get(:@config) if connection
+        end
       end
     end
-
   end
 end
 
@@ -73,7 +74,7 @@ TingYun::Support::LibraryDetection.defer do
   end
 
   depends_on do
-    !TingYun::Instrumentation::ActiveRecordSubscriber.subscribed?
+    !TingYun::Instrumentation::Rails4::ActiveRecordSubscriber.subscribed?
   end
 
   executes do
@@ -82,7 +83,7 @@ TingYun::Support::LibraryDetection.defer do
 
   executes do
     ActiveSupport::Notifications.subscribe('sql.active_record',
-                                           TingYun::Instrumentation::ActiveRecordSubscriber.new)
+                                           TingYun::Instrumentation::Rails4::ActiveRecordSubscriber.new)
     ::TingYun::Instrumentation::Support::ActiveRecordHelper.instrument_writer_methods
   end
 end
