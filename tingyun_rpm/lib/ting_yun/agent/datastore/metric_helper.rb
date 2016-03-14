b27a143118f940223145d83dc32879fc8c8b986e
anehing <anehing@gmail.com>
2016-03-14 20:03:11 +0800

    增加show

--- tingyun_rpm/lib/ting_yun/agent/datastore/metric_helper.rb
+++ tingyun_rpm/lib/ting_yun/agent/datastore/metric_helper.rb
@@ -18,9 +18,9 @@ module TingYun
 
         def self.metric_name(product, collection, operation)
           if checkNosql(product)
-            "#{product}/#{collection}/#{operation}"
+            "#{product}/#{collection||"NULL"}/#{operation}"
           else
-            "Database #{product}/#{collection}/#{operation}"
+            "Database #{product}/#{collection||"NULL"}/#{operation}"
           end
         end
 
@@ -35,10 +35,7 @@ module TingYun
 
 
         def self.metrics_for(product, operation, collection = nil, generic_product = nil)
-          return nil if operation.nil?
-
           operation = operation.to_s.upcase
-
           if overrides = overridden_operation_and_collection   # [method, model_name, product]
             if should_override?(overrides, product, generic_product)
               operation  = overrides[0] || operation
