70a8f233a33c3280bda653ee95b6dd5a22738e39
liuhq <liuhq@ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da>
2016-06-21 07:26:42 +0000

    transactionid 3级别以上跨越
    git-svn-id: https://192.168.1.42/tingyun-ruby-agent@212 ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da

--- tingyun_rpm/lib/ting_yun/agent/cross_app/cross_app_tracing.rb
+++ tingyun_rpm/lib/ting_yun/agent/cross_app/cross_app_tracing.rb
@@ -158,7 +158,7 @@ module TingYun
         cross_app_id  = TingYun::Agent.config[:tingyunIdSecret] or
             raise TingYun::Agent::CrossAppTracing::Error, "no tingyunIdSecret configured"
 
-        txn_guid = state.request_guid
+        txn_guid =  state.client_transaction_id || state.request_guid
         state.transaction_sample_builder.trace.tx_id = txn_guid
         request[TY_ID_HEADER] = "#{cross_app_id};c=1;x=#{txn_guid}"
 
