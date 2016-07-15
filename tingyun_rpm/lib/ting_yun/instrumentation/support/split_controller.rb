651713ac04e9ca21392b43df204619f6a043511a
liuhq <liuhq@ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da>
2016-07-15 05:36:55 +0000

    1.1.0
    git-svn-id: https://192.168.1.42/tingyun-ruby-agent@244 ce082a47-4e5e-4a2f-b045-a6c6c2a1a5da

--- tingyun_rpm/lib/ting_yun/instrumentation/support/split_controller.rb
+++ tingyun_rpm/lib/ting_yun/instrumentation/support/split_controller.rb
@@ -2,48 +2,48 @@ module TingYun
   module Instrumentation
     module Support
       module SplitController
-        HTTP = {
-          'GET' => 1,
-          'POST' => 2,
-          'PUT' => 3,
-          'DELETE' => 4,
-          'HEAD' => 5
-        }
-
-        RULE = {
-          1=> :eql?,
-          2=> :start_with?,
-          3=> :end_with?,
-          4=> :include?
-        }
-
-        def match?(event)
-          rules.each do |rule|
-            if method_match?(event.method, rule[:match]['method'])
-              if url_match?(event.path,rule[:match][:match], rule[:match]['value'])
-
-              end
-            end
-          end
-        end
-
-
-
-        def rules
-          TingYun::Agent.config[:'']
-        end
-
-        def method_match?(method, rule)
-          rule[:match]['method'] == 0 || rule == HTTP[method]
-        end
-
-        def url_match?(url, rule, value)
-          url.send(RULE[rule], value)
-        end
-
-        def params_match?
-
-        end
+        # HTTP = {
+        #   'GET' => 1,
+        #   'POST' => 2,
+        #   'PUT' => 3,
+        #   'DELETE' => 4,
+        #   'HEAD' => 5
+        # }
+        #
+        # RULE = {
+        #   1=> :eql?,
+        #   2=> :start_with?,
+        #   3=> :end_with?,
+        #   4=> :include?
+        # }
+        #
+        # def match?(event)
+        #   rules.each do |rule|
+        #     if method_match?(event.method, rule[:match]['method'])
+        #       if url_match?(event.path,rule[:match][:match], rule[:match]['value'])
+        #
+        #       end
+        #     end
+        #   end
+        # end
+        #
+        #
+        #
+        # def rules
+        #   TingYun::Agent.config[:'']
+        # end
+        #
+        # def method_match?(method, rule)
+        #   rule[:match]['method'] == 0 || rule == HTTP[method]
+        # end
+        #
+        # def url_match?(url, rule, value)
+        #   url.send(RULE[rule], value)
+        # end
+        #
+        # def params_match?
+        #
+        # end
       end
     end
   end
