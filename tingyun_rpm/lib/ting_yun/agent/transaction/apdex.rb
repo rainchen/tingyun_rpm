b3e9cc769e723ca940d73c5005a572d6001931c3
anehing <anehing@gmail.com>
2016-08-19 17:13:59 +0800

    time now

--- tingyun_rpm/lib/ting_yun/agent/transaction/apdex.rb
+++ tingyun_rpm/lib/ting_yun/agent/transaction/apdex.rb
@@ -9,8 +9,8 @@ module TingYun
 
         attr_accessor :apdex_start, :transaction_start_time
 
-        def initialize(start = Time.now, transaction_start)
-          @apdex_start = start
+        def initialize(start, transaction_start)
+          @apdex_start = start || Time.now
           @transaction_start_time = transaction_start
         end
 
