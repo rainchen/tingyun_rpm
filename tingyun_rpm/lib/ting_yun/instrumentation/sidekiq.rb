c10095c5e00a44089661e3cb136917cc2158c8ee
tingyun-ruby-agent <liuhq@tingyun.com>
2017-02-20 15:13:38 +0800

    sidekiq

--- tingyun_rpm/lib/ting_yun/instrumentation/sidekiq.rb
+++ tingyun_rpm/lib/ting_yun/instrumentation/sidekiq.rb
@@ -17,8 +17,38 @@ TingYun::Support::LibraryDetection.defer do
 
   executes do
     class TingYun::SidekiqInstrumentation
-      def call(worker_instance, msg, queue,*_)
+      include TingYun::Instrumentation::Support::ControllerInstrumentation
+      def call(worker_instance, msg, queue, *_)
+        trace_args = if worker_instance.respond_to?(:tingyun_trace_args)
+                       worker_instance.tingyun_trace_args(msg, queue)
+                     else
+                       self.class.default_trace_args(msg)
+                     end
+        perform_action_with_tingyun_trace(trace_args) do
+          yield
+        end
+      end
 
+      def self.default_trace_args(msg)
+        {
+            :name => 'perform',
+            :class_name => msg['class'],
+            :category => 'BackgroundAction/SidekiqJob'
+        }
+      end
+    end
+
+    class Sidekiq::Extensions::DelayedClass
+      def tingyun_trace_args(msg, queue)
+        (target, method_name, _args) = YAML.load(msg['args'][0])
+        {
+            :name => method_name,
+            :class_name => target.name,
+            :category => 'BackgroundAction/SidekiqJob'
+        }
+      rescue => e
+        TingYun::Agent.logger.error("Failure during deserializing YAML for Sidekiq::Extensions::DelayedClass", e)
+        TingYun::SidekiqInstrumentation.default_trace_args(msg)
       end
     end
 
