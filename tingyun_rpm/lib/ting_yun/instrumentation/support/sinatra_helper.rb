3ad3e98721290705471ea883f37593d655d1a013
wangst <wangst@tingyun.com>
2017-02-24 11:32:55 +0800

    sinatra 1.4.8

--- tingyun_rpm/lib/ting_yun/instrumentation/support/sinatra_helper.rb
+++ tingyun_rpm/lib/ting_yun/instrumentation/support/sinatra_helper.rb
@@ -8,7 +8,7 @@ module TingYun
         module_function
 
         SINATRA_MIN_VERSION = '1.2.3'.freeze
-        SINATRA_MAX_VERSION = '1.4.7'.freeze
+        SINATRA_MAX_VERSION = '1.4.8'.freeze
 
         def version_supported?
           TingYun::Support::VersionNumber.new(::Sinatra::VERSION) >= TingYun::Support::VersionNumber.new(SINATRA_MIN_VERSION)
