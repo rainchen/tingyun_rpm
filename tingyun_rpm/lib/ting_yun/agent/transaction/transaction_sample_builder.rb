5bed920c26c5907e42474f441a70339e9f851366
tingyun-ruby-agent <liuhq@tingyun.com>
2017-11-13 16:38:39 +0800

    action_trace里的exception变成数组

--- tingyun_rpm/lib/ting_yun/agent/transaction/transaction_sample_builder.rb
+++ tingyun_rpm/lib/ting_yun/agent/transaction/transaction_sample_builder.rb
@@ -72,10 +72,10 @@ module TingYun
           @current_node = @current_node.parent_node
         end
         if error
-          @current_node["exception"] = {"message" => error.message,
+          @current_node["exception"] = [{"message" => error.message,
                                         "class" => error.class.to_s,
                                         "stacktrace"=> error.backtrace
-          }
+          }]
         end
 
       end
